ae4e28b16cf4b93a60f8049aa4040a7dc9cf1969
Luis Villarreal <luisvillarrealx@gmail.com>
2022-06-18 18:56:53 -0600

    Agregar arte

---
+++ app/src/main/java/com/galartt/ui/galeria/AddArteFragment.kt
@@ -0,0 +1,56 @@
+package com.galartt.ui.galeria
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.galartt.R
+import com.galartt.databinding.FragmentAddArteBinding
+import com.galartt.viewmodel.GaleriaViewModel
+
+// FragmentAddArteBinding = FragmentAddGaleriaBinding
+
+class AddArteFragment : Fragment() {
+
+    private lateinit var galeriaViewModel: GaleriaViewModel
+
+    private var _binding: FragmentAddArteBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        galeriaViewModel = ViewModelProvider(this)[GaleriaViewModel::class.java]
+        _binding = FragmentAddArteBinding.inflate(inflater, container,false)
+
+        // agregar un lugar
+        binding.btAdd.setOnClickListener { addArte() }
+
+        return binding.root
+    }
+
+    private fun addArte() {
+        val autor = binding.etAutor.text.toString()
+        val correo = binding.etCorreo.text.toString()
+        val telefono = binding.etTelefono.text.toString()
+        val web = binding.etWeb.text.toString()
+        if (autor.isNotEmpty()) {
+            val arte = Galeria(0, autor, correo, telefono, web, 0.0, 0.0, 0.0, "", "")
+            galeriaViewModel.addArte(arte)
+            Toast.makeText(requireContext(), getString(R.string.arteAdded), Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_addArteFragment_to_nav_galeria)
+        } else {
+            Toast.makeText(requireContext(), getString(R.string.notAdded), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/com/galartt/ui/galeria/GaleriaFragment.kt
+++ app/src/main/java/com/galartt/ui/galeria/GaleriaFragment.kt
@@ -4,36 +4,41 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.galartt.R
 import com.galartt.databinding.FragmentGaleriaBinding
 import com.galartt.viewmodel.GaleriaViewModel
 
+// FragmentGaleriaBinding = FragmentArteBinding
+
 class GaleriaFragment : Fragment() {
 
-    private var _binding: FragmentGaleriaBinding? = null
+    private lateinit var galeriaViewModel: GaleriaViewModel
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentGaleriaBinding? = null
     private val binding get() = _binding!!
 
+/* 1:17:00  https://ufidelitas.sharepoint.com/sites/ProgramacinparaDispositivosMvilesCampusVirtual2022
+Segundo712/Documentos%20compartidos/Forms/AllItems.aspx?id=%2Fsites%2FProgramacinparaDispositivosMviles
+CampusVirtual2022Segundo712%2FDocumentos%20compartidos%2FGeneral%2FRecordings%2FClase%20Sincr%C3%B3nica%20
+M%C3%B3vil%20Jueves%20Noche%2D20220602%5F181058%2DMeeting%20Recording%2Emp4&parent=%2Fsites%2FProgramacinpara
+DispositivosMvilesCampusVirtual2022Segundo712%2FDocumentos%20compartidos%2FGeneral%2FRecordings*/
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val galeriaViewModel =
-            ViewModelProvider(this).get(GaleriaViewModel::class.java)
+    ): View? {
+        galeriaViewModel = ViewModelProvider(this)[GaleriaViewModel::class.java]
+        _binding = FragmentGaleriaBinding.inflate(inflater, container,false)
 
-        _binding = FragmentGaleriaBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        galeriaViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        // accion para pasar a addObra
+        binding.addArteButton.setOnClickListener{
+            findNavController().navigate(R.id.action_nav_galeria_to_addArteFragment)
         }
-        return root
+
+        return binding.root
     }
 
     override fun onDestroyView() {
